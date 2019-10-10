adc9ebe3d0cb268997a03303943ed7fd18d7c27d
liuhongyin <liuhongyin@7761@gmail.com>
2019-10-10 22:21:12 +0800

    first commit

---
+++ src/main/java/com/lliuhongyin/mybatislearn/MybatisLearnApplication.java
@@ -0,0 +1,13 @@
+package com.lliuhongyin.mybatislearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisLearnApplication.class, args);
+    }
+
+}
